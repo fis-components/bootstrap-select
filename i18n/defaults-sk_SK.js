0b9fb6eed4b2e0bddb100a00a748bac01d9139ae
xiangshouding <fansekey@gmail.com>
2015-06-24 06:37:15 +0000

    based on https://github.com/fis-components/components/blob/master/modules/bootstrap-select.js

---
+++ i18n/defaults-sk_SK.js
@@ -0,0 +1,33 @@
+/*!
+ * Bootstrap-select v1.7.3 (http://silviomoreto.github.io/bootstrap-select)
+ *
+ * Copyright 2013-2015 bootstrap-select
+ * Licensed under MIT (https://github.com/silviomoreto/bootstrap-select/blob/master/LICENSE)
+ */
+
+(function (root, factory) {
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    
+    
+    var a0 = require('jquery');
+    module.exports = factory(a0) || module.exports;;
+}(this, function () {
+    (function ($) {
+        $.fn.selectpicker.defaults = {
+            noneSelectedText: 'Vyberte zo zoznamu',
+            noneResultsText: 'Pre v\xFDraz {0} neboli n\xE1jden\xE9 \u017Eiadne v\xFDsledky',
+            countSelectedText: 'Vybran\xE9 {0} z {1}',
+            maxOptionsText: [
+                'Limit prekro\u010Den\xFD ({n} {var} max)',
+                'Limit skupiny prekro\u010Den\xFD ({n} {var} max)',
+                [
+                    'polo\u017Eiek',
+                    'polo\u017Eka'
+                ]
+            ],
+            selectAllText: 'Vybra\u0165 v\u0161etky',
+            deselectAllText: 'Zru\u0161i\u0165 v\xFDber',
+            multipleSeparator: ', '
+        };
+    }(jQuery));
+}));
---
+++ i18n/defaults-sl_SI.js
@@ -0,0 +1,33 @@
+/*!
+ * Bootstrap-select v1.7.3 (http://silviomoreto.github.io/bootstrap-select)
+ *
+ * Copyright 2013-2015 bootstrap-select
+ * Licensed under MIT (https://github.com/silviomoreto/bootstrap-select/blob/master/LICENSE)
+ */
+
+(function (root, factory) {
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    
+    
+    var a0 = require('jquery');
+    module.exports = factory(a0) || module.exports;;
+}(this, function () {
+    (function ($) {
+        $.fn.selectpicker.defaults = {
+            noneSelectedText: 'Ni\u010D izbranega',
+            noneResultsText: 'Ni zadetkov za {0}',
+            countSelectedText: function (numSelected, numTotal) {
+                '\u0160tevilo izbranih: {0}';
+            },
+            maxOptionsText: function (numAll, numGroup) {
+                return [
+                    'Omejitev dose\u017Eena (max. izbranih: {n})',
+                    'Omejitev skupine dose\u017Eena (max. izbranih: {n})'
+                ];
+            },
+            selectAllText: 'Izberi vse',
+            deselectAllText: 'Po\u010Disti izbor',
+            multipleSeparator: ', '
+        };
+    }(jQuery));
+}));
