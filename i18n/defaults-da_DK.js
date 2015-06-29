5836fd630bbce41e392baf09442b89d525a2b9af
xiangshouding <fansekey@gmail.com>
2015-06-29 07:04:36 +0000

    based on https://github.com/fis-components/components/blob/master/modules/bootstrap-select.js

---
+++ i18n/defaults-da_DK.js
@@ -0,0 +1,33 @@
+/*!
+ * Bootstrap-select v1.7.3 (http://silviomoreto.github.io/bootstrap-select)
+ *
+ * Copyright 2013-2015 bootstrap-select
+ * Licensed under MIT (https://github.com/silviomoreto/bootstrap-select/blob/master/LICENSE)
+ */
+
+(function (root, factory) {
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    
+    
+    var a0 = require('jquery');
+    module.exports = factory(a0) || module.exports;;
+}(this, function () {
+    (function ($) {
+        $.fn.selectpicker.defaults = {
+            noneSelectedText: 'Intet valgt',
+            noneResultsText: 'Ingen resultater fundet {0}',
+            countSelectedText: function (numSelected, numTotal) {
+                return numSelected == 1 ? '{0} valgt' : '{0} valgt';
+            },
+            maxOptionsText: function (numAll, numGroup) {
+                return [
+                    numAll == 1 ? 'Begr\xE6nsning n\xE5et (max {n} valgt)' : 'Begr\xE6nsning n\xE5et (max {n} valgte)',
+                    numGroup == 1 ? 'Gruppe-begr\xE6nsning n\xE5et (max {n} valgt)' : 'Gruppe-begr\xE6nsning n\xE5et (max {n} valgte)'
+                ];
+            },
+            selectAllText: 'Mark\xE9r alle',
+            deselectAllText: 'Afmark\xE9r alle',
+            multipleSeparator: ', '
+        };
+    }(jQuery));
+}));
---
+++ i18n/defaults-it_IT.js
@@ -0,0 +1,31 @@
+/*!
+ * Bootstrap-select v1.7.3 (http://silviomoreto.github.io/bootstrap-select)
+ *
+ * Copyright 2013-2015 bootstrap-select
+ * Licensed under MIT (https://github.com/silviomoreto/bootstrap-select/blob/master/LICENSE)
+ */
+
+(function (root, factory) {
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    
+    
+    var a0 = require('jquery');
+    module.exports = factory(a0) || module.exports;;
+}(this, function () {
+    (function ($) {
+        $.fn.selectpicker.defaults = {
+            noneSelectedText: 'Nessuna selezione',
+            noneResultsText: 'Nessun risultato per {0}',
+            countSelectedText: 'Selezionati {0} di {1}',
+            maxOptionsText: [
+                'Limite raggiunto ({n} {var} max)',
+                'Limite del gruppo raggiunto ({n} {var} max)',
+                [
+                    'elementi',
+                    'elemento'
+                ]
+            ],
+            multipleSeparator: ', '
+        };
+    }(jQuery));
+}));
